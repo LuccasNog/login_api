f763a3f6b91de4b921ec27f71f1a1c01191864bd
Lucas Nogueira <nozescrypto@gmail.com>
2023-04-24 16:21:16 -0300

    correction page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,23 +10,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-//Página vai iniciar
-  @override
-  void initState() {
-    super.initState();
-    // Verificar há o token no inicio da página
-    // Se o valor for true, vai pra página Homepage
-    verirfyToken().then((value) {
-      if (value) {
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => HomePage()));
-      } else {
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => LoginPage()));
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,17 +17,5 @@ class _LoginPageState extends State<LoginPage> {
         child: CircularProgressIndicator(),
       ),
     );
-  }
-
-  // Token responsável pela conexão com API
-  //vamos verificar se há usuário ou não
-  Future<bool> verirfyToken() async {
-    //SharedPreferences vai verificar se temos token
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    if (sharedPreferences.getString('token') != null) {
-      return true;
-    } else {
-      return false;
-    }
   }
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'home_page.dart';
+import 'login_page.dart';
 
 class Boasvindas extends StatefulWidget {
   const Boasvindas({super.key});
@@ -8,8 +12,39 @@ class Boasvindas extends StatefulWidget {
 }
 
 class _BoasvindasState extends State<Boasvindas> {
+  void initState() {
+    super.initState();
+    // Verificar há o token no inicio da página
+    // Se o valor for true, vai pra página Homepage
+    verirfyToken().then((value) {
+      if (value) {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      } else {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  // Token responsável pela conexão com API
+  //vamos verificar se há usuário ou não
+  Future<bool> verirfyToken() async {
+    //SharedPreferences vai verificar se temos token
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    if (sharedPreferences.getString('token') != null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
